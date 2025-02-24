19d167053e4f7f1892004e98d19f9be428a1bca8
ANTEUR3 <anteuryounes1@gmail.com>
2025-02-24 15:15:50 +0100

    create class RGB to store RGB values in its objects to generate random RGB values for points inside sugments

---
+++ src/sugmentgeneration/RGB.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sugmentgeneration;
+
+/**
+ *
+ * @author Admin
+ */
+public class RGB {
+    public int R;
+    public int G;
+    public int B;
+    
+    
+    public RGB(int r,int g,int b){
+        this.R=r;
+        this.G=g;
+        this.B=b;
+
+    }
+
+    
+}
